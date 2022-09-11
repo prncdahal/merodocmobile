f71a16f7b600087ea21e8023b4a5aa672a3aecaf
Ramesh Dahal <prncdahal@gmail.com>
2022-09-11 17:31:07 +0545

    add family form added

--- js/main.js
+++ js/main.js
@@ -1,39 +1,35 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  $(".billboard").slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    dots: true,
+  });
 
-    $('.billboard').slick({
-        infinite:true,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        arrows:false,
-        dots:true
-    });
+  // Toggle on check
+  $("#showMap").change(function () {
+    if (this.checked) {
+      $(".map-range").addClass("d-block");
+    } else {
+      $(".map-range").removeClass("d-block");
+    }
+  });
 
-    // Toggle on check
-    $("#showMap").change(function() {
-        if(this.checked) {
-           $('.map-range').addClass('d-block');
-        } else{
-            $('.map-range').removeClass('d-block');
-        }
-    });
-
-    // $(document).bind("contextmenu", function (e) {
-    //     return false;
-    // });
-
+  // $(document).bind("contextmenu", function (e) {
+  //     return false;
+  // });
 });
 // Floating button Effects
 var lastScrollTop = 0;
-$(window).scroll(function(event){
-   var st = $(this).scrollTop();
-   if (st > lastScrollTop){
+$(window).scroll(function (event) {
+  var st = $(this).scrollTop();
+  if (st > lastScrollTop) {
     // downscroll code
-    $('.floating-btn').addClass('open')
-       
-   } else {
-      // upscroll code
-      $('.floating-btn').removeClass('open')
-   }
-   lastScrollTop = st;
+    $(".floating-btn").addClass("open");
+  } else {
+    // upscroll code
+    $(".floating-btn").removeClass("open");
+  }
+  lastScrollTop = st;
 });
-
